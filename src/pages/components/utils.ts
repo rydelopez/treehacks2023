4e85ea18bd75c41e54c947b9f49e75d2f25a0e26
Alex Cho <104597806+aychocho@users.noreply.github.com>
2023-02-18 16:15:49 -0500

    API WORKSSSSSSSSSS

--- src/pages/components/utils.ts
+++ src/pages/components/utils.ts
@@ -1,4 +1,5 @@
-import { AptosClient, TokenClient, AptosAccount } from "aptos";
+import { AptosClient, TokenClient, AptosAccount, HexString} from "aptos";
+import { Wallet } from "@aptos-labs/wallet-adapter-core";
 
 /**
  * Reference:
@@ -10,12 +11,14 @@ const admin_address = "0x76c6703811ecfc91ca761600b782ae3cd1a9845c3ca940f14225a49
 
 const hexString = "0xcdf5f74f8504b15f1b08209b1ba5c67fe12d2a11d613d984458d16f37ac43d0a"
 
-var bytes = new Uint8Array(Math.ceil(hexString.length / 2));
-for (var i = 0; i < bytes.length; i++) bytes[i] = parseInt(hexString.substr(i * 2, 2), 16);
-const admin_pk = bytes;
+const admin_pk = new HexString("0xcdf5f74f8504b15f1b08209b1ba5c67fe12d2a11d613d984458d16f37ac43d0a").toUint8Array();
+
+
+const aptosAccount = new AptosAccount()
+
 
 const defaultNetworks = {
-    nodeUrl: 'https://fullnode.devnet.aptoslabs.com'
+    nodeUrl: 'https://fullnode.testnet.aptoslabs.com'
 }
 
 function getAptosClient() {
@@ -28,15 +31,37 @@ export async function getAccountResources(address) {
     return accountResources;
 }
 
-export function createToken(address, name, SerialNum, description, img) {
+export async function createToken(address, name, SerialNum, description, img) {
     const aptosClient = getAptosClient();
     const tokenClient = new TokenClient(getAptosClient());
-    const adminAccount = new AptosAccount(admin_pk, admin_address);
-    tokenClient.createCollection(adminAccount, name, description, img, 1);
-    const hash = tokenClient.createToken(adminAccount, name, SerialNum, description, 1, img, 1);
+    const adminAccount = new AptosAccount(admin_pk);
+	await aptosClient.waitForTransaction(
+		await tokenClient.createCollection(adminAccount, SerialNum, description, img, 1), {checkSuccess: true},
+	);
+	console.log(
+		"collection:" + tokenClient.getCollectionData(address, SerialNum)
+	);
+
+	const hash = await tokenClient.createToken(adminAccount, SerialNum, name, description, 1, img, 1);
+	await aptosClient.waitForTransaction(
+		hash, {checkSuccess: true},
+	);
+	console.log(
+		"minting hash: " , hash
+	);
+
+	const offerHash = await tokenClient.offerToken(adminAccount, address, admin_address, SerialNum, name, 1);
+	await aptosClient.waitForTransaction(
+	offerHash, {checkSuccess: true},
+	);
+
+	console.log('offerHash ', offerHash)
+	const collectionData = await tokenClient.getCollectionData(adminAccount.address(), SerialNum);
+	console.log(`new collection: ${JSON.stringify(collectionData, null, 4)}`); 
+	
 }
 
-export function sendToken(address, name, SerialNum, dest) {
+export async function sendToken(address, name, SerialNum, dest) {
 	const tokenClient = new TokenClient(getAptosClient());
-	tokenClient.offerToken(address, dest, admin_address, name, SerialNum, 1);
+	tokenClient.offerToken(address, dest, admin_address, SerialNum, name, 1);
 }
