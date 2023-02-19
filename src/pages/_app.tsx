93e72656d6cd8c949b59be700de685a22f50e439
Alex Cho <104597806+aychocho@users.noreply.github.com>
2023-02-18 20:53:52 -0500

    account from petra works!

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,26 @@ import { PetraWallet } from "petra-plugin-wallet-adapter";
 import { AuthContextProvider } from "./lib/AuthContext";
 import { useRouter } from "next/router";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { useEffect, useState } from "react";
+
+declare global {
+  interface Window { MyNamespace: any; }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
 	const wallets = [new PetraWallet()];
 	const noAuthRequired = ["/login", "/signup"];
 	const router = useRouter();
+  const [account ,setAccount ] = useState<any>(undefined);
+
+  console.log(account);
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      // @ts-ignore
+      window.aptos.account().then((data) => setAccount(data))
+    }
+  }, [])
 
 	return (
 		<AuthContextProvider>
