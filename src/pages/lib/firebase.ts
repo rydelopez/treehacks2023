c6231a0a74f1162fe236047998e84f0d8221a55e
Ryan De Lopez <delopezryan@gmail.com>
2024-12-01 11:02:39 -0500

    Update firebase.ts

--- src/pages/lib/firebase.ts
+++ src/pages/lib/firebase.ts
@@ -9,15 +9,6 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-	apiKey: "AIzaSyBNCZpxR1-x63GqB0scStVuz6NVULwMT28",
-	authDomain: "certiflow-nft.firebaseapp.com",
-	projectId: "certiflow-nft",
-	storageBucket: "certiflow-nft.appspot.com",
-	messagingSenderId: "518423014075",
-	appId: "1:518423014075:web:21812162788469efcb979e",
-	measurementId: "G-3XJ5JY834C",
-};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
